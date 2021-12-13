cdf4fb25ffba3dab8a72675ee1da18facc6290d0
Leon <leonkluska0@gmail.com>
2021-12-13 22:00:49 +0100

    second commit

--- hanoi/sketch.js
+++ hanoi/sketch.js
@@ -1,19 +1,58 @@
+class Hitbox{
+   constructor(x, y, width, height){
+     this.x = x;
+     this.y = y;
+     this.width = width;
+     this.height = height;
+   }
+   drawHitbox(){
+     if(!debug){
+       noStroke();
+     }
+     fill(backgroundColor);
+     rect(this.x, this.y, this.width, this.height);
+   }
+   clicked(){
+         return (mouseX > this.x) &&
+        (mouseX < (this.x + this.width)) &&
+        (mouseY > this.y) &&
+        (mouseY < (this.y + this.height));
+   }
+ }
+
  class Stick{
-  constructor(x1,y1,x2,y2, rings){
+  constructor(x1, y1, x2, y2, amountOfRings){
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
-    this.rings = rings;
+    this.rings = [];
+    this.hitbox = new Hitbox(x1-(hitboxWidth/2),y1-hitboxHeight, hitboxWidth,hitboxHeight);
+    this.createRings(amountOfRings);
   }
   drawStick(){
+    this.hitbox.drawHitbox();
+    stroke(0);
     line(this.x1,this.y1,this.x2,this.y2);
+    for(let i = 0; i < this.rings.length; i++){
+      this.rings[i].drawRing();
+    }
+  }
+  createRings(amountOfRings){
+    let width = 200;
+    for(let i = 0; i < amountOfRings; i++){
+      this.addRing(createRing(width));
+      width -= 30;
+    }
   }
   numberOfRings(){
     return this.rings;
   }
-  addRing(){
-    this.rings++;
+  addRing(ring){
+    this.rings.push(ring);
+  }
+  peek(){
+    return this.rings[this.rings.length-1];
   }
   stickX(){
     return this.x1;
@@ -21,8 +60,33 @@
   stickY(){
     return this.y1;
   }
-}
+  clicked(){
+      if(this.hitbox.clicked()){
+        if (currentMovingRing) {
+          if(this.rings.length == 0 || this.peek().ringWidth() > currentMovingRing.ringWidth()) {
+            this.addRing(currentMovingRing);
+            currentMovingRing = null
+          }
+            else {
+              print("nie możesz")
+            }
+        } else {
+          currentMovingRing = this.pop();
+        }
+    }
+  }
+  pop(){
+    return this.rings.pop();
+  }
+  updateRings(){
+    for (let i = 0; i<this.rings.length;i++){
+        this.rings[i].moveRing(this.stickX()-(this.rings[i].ringWidth()/2),this.stickY()-ringHeight);
+        ringHeight+=20
+    }
+    ringHeight=20;
+  }
 
+}
 class Ring{
   constructor(x, y ,width, height){
     this.x = x;
@@ -33,6 +97,7 @@ class Ring{
 
   }
   drawRing(){
+    stroke(0);
     fill(this.col)
     rect(this.x,this.y,this.width,this.height)
   }
@@ -44,140 +109,86 @@ class Ring{
   ringHeight(){
     return this.height;
   }
-  changeWidth(number){
-    this.width -= number;
+  setWidth(number){
+    this.width = number;
   }
   ringWidth(){
     return this.width;
   }
-  clicked(){
-        return (mouseX > this.x) &&
-       (mouseX < (this.x*this.width)) &&
-       (mouseY > this.y) &&
-       (mouseY < (this.y * this.height))
-  }
-
 }
 
-let ring = []
-let stick =[]
-ringWidth = 200;
-ringHeight = 20;
-ringHeightConst = 20;
-ringwidthConst = 200;
-currentMovingRing = 0;
-widthToSubtract = 30;
-sticks = 3;
-ringToMove = 0;
-mouseIsBusy = false;
-rings=3;
+let ring = [];
+let stick = [];
+let button;
+let ringWidth = 200;
+let ringHeight = 20;
+let ringHeightConst = 20;
+let ringwidthConst = 200;
+let currentMovingRing;
+let sticks = 3;
+let rings = 7;
+let backgroundColor = 150;
+let hitboxWidth = 180;
+let hitboxHeight = 200;
+
+let debug = false;
+
+function createSticks(){
+  stick[0] = new Stick(stickX,600,stickX,400, rings)
+  stick[1] = new Stick(stickX+200,600,stickX+200,400,0);
+  stick[2] = new Stick(stickX+400,600,stickX+400,400,0);
+}
 
 function drawSticks(){
-  for (let i = 0; i<sticks;i++){
+  for (i = 0; i < sticks; i++){
     stick[i].drawStick();
   }
 }
 
-function drawRings(){
-  for (let i = 0; i<rings;i++){
-      ring[i].drawRing();
+function updateStick(){
+  for (i = 0; i < sticks; i++){
+    stick[i].updateRings();
   }
 }
 
-function mouseReleased() {
-  ringToMove=0;
+function updateCurrentRing(){
+  if(currentMovingRing){
+    currentMovingRing.drawRing();
+    currentMovingRing.moveRing(mouseX - currentMovingRing.width / 2 ,mouseY - currentMovingRing.height/2);
+  }
 }
 
-function tripleRing(){
-  setTimeout(() => {
-    ring[2].moveRing(stick[2].stickX()-(ring[2].ringWidth()/2),height-ringHeightConst);
-    }, 1000);
-   setTimeout(() => {
-     ring[1].moveRing(stick[1].stickX()-(ring[1].ringWidth()/2),height-ringHeightConst);
-    }, 2000);
-   setTimeout(() => {
-     ring[2].moveRing(stick[1].stickX()-(ring[2].ringWidth()/2),height-ringHeightConst*2);
-    }, 3000);
-  setTimeout(() => {
-      ring[0].moveRing(stick[2].stickX()-(ring[0].ringWidth()/2),height-ringHeightConst);
-    }, 4000);
-  setTimeout(() => {
-      ring[2].moveRing(stick[0].stickX()-(ring[2].ringWidth()/2),height-ringHeightConst);
-    }, 5000);
-  setTimeout(() => {
-      ring[1].moveRing(stick[2].stickX()-(ring[1].ringWidth()/2),height-ringHeightConst*2);
-    }, 6000);
-  setTimeout(() => {
-      ring[2].moveRing(stick[2].stickX()-(ring[2].ringWidth()/2),height-ringHeightConst*3);
-    }, 7000);
+function createRing(width){
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    return new Ring(300,300, width, ringHeight);
 }
 
-function fourRings(){
-  rings = 4;
+function mouseClicked() {
+  for (i = 0; i < sticks; i++){
+    stick[i].clicked();
+  }
 }
 
-function reset(){
-
+function win(){
+  if(stick[2].rings.length == rings){
+    print("chuj");
+  }
 }
-
-let button;
-let button2;
-let button3;
 
 function setup() {
   createCanvas(800, 600);
   rectX = 200;
   rectY = 20;
   stickX = 200;
-  button = createButton("solve!");
-  button.position(100,100);
-  button.size(100,60);
-  button.mousePressed(tripleRing);
-
-  // button2 = createButton("4 Rings");
-  // button2.position(300,100);
-  // button2.size(100,60);
-  // button2.mousePressed(fourRings)
-  //
-  // button3 = createButton("Reset");
-  // button3.position(600,100);
-  // button3.size(100,60);
-  // button3.mousePressed(reset);
-
-  for(let i = 0; i<3;i++){
-    stick[i] = new Stick(stickX,600,stickX,400,0);
-    stickX+=200;
-  }
-  for (let i = 0; i<rings;i++){
-    r = random(255);
-    g = random(255);
-    b = random(255);
-    ring[i] = new Ring(300,300, ringWidth, ringHeight);
-  }
-  stick[0].addRing();
-  stick[0].addRing();
-  stick[0].addRing();
-
- for (let i = 0; i<rings;i++){
-      ring[i].moveRing(stick[0].stickX()-(ringWidth/2)+(widthToSubtract/2),stick[0].stickY()-ringHeight);
-      ring[i].changeWidth(widthToSubtract);
-      widthToSubtract+=30
-      ringHeight+=20
-  }
+  createSticks();
 }
 
 function draw() {
-  background(150);
+  background(backgroundColor);
   drawSticks();
-  drawRings();
-  if(mouseIsPressed){
-      for (let i = 0; i<rings;i++){
-      if(ring[i].clicked()==true){
-        ringToMove = ring.indexOf(ring[i]);
-        mouseIsBusy = true;
-      }
-    }
-    //  ring[ringToMove].moveRing(mouseX,mouseY);
-  }
-
+  updateStick();
+  updateCurrentRing();
+  win();
 }
